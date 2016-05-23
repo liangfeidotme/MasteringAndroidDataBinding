2343ddd93b3385f8858a31453aca21161509bdd2
喵叔catuncle <andych008@163.com>
2016-05-23 18:20:23 +0800

    the way to get included widgets by id

--- app/src/main/java/com/liangfeizc/databinding/sample/include/IncludeActivity.java
+++ app/src/main/java/com/liangfeizc/databinding/sample/include/IncludeActivity.java
@@ -2,25 +2,43 @@ package com.liangfeizc.databinding.sample.include;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.liangfeizc.databinding.R;
-import com.liangfeizc.databinding.sample.BaseActivity;
 import com.liangfeizc.databinding.databinding.ActivityIncludeBinding;
-import com.liangfeizc.databinding.model.Contact;
 import com.liangfeizc.databinding.model.User;
+import com.liangfeizc.databinding.sample.BaseActivity;
 
 public class IncludeActivity extends BaseActivity {
+
+    private ActivityIncludeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityIncludeBinding binding = DataBindingUtil.setContentView(
+        binding = DataBindingUtil.setContentView(
                 this, R.layout.activity_include);
-        User user = new User("liang", "fei");
-        Contact contact = new Contact("123456", "China");
 
-        binding.setUser(user);
-        binding.setContact(contact);
+        //in order to get the etName, you must define an id for the include tag.
+        binding.layoutInput.etName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                User user = new User(s.toString(), "Liang");
+
+                binding.setUser(user);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
 
