4a1e8ffd788f3c0a04b2d97dd326b621bc376477
liangfei <njliangfei@gmail.com>
2019-01-20 00:12:26 +0800

    add drawer

--- app/src/main/java/me/liangfei/databinding/sample/MainActivity.java
+++ app/src/main/java/me/liangfei/databinding/sample/MainActivity.java
@@ -2,7 +2,12 @@ package me.liangfei.databinding.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import me.liangfei.databinding.R;
 import me.liangfei.databinding.sample.attributesetter.AttributeSettersActivity;
 import me.liangfei.databinding.sample.basic.BasicActivity;
@@ -16,14 +21,54 @@ import me.liangfei.databinding.sample.resource.ResourceActivity;
 import me.liangfei.databinding.sample.viewid.ViewWithIDsActivity;
 import me.liangfei.databinding.sample.viewstub.ViewStubActivity;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void openBasic(View view) {
