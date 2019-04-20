9aca35c4de4511bfc0846459cc6ab2f88fcbfb4f
liangfei <njliangfei@gmail.com>
2019-04-20 11:30:46 +0800

    add + icon in actor list

--- app/src/main/java/me/liangfei/databinding/adapters/ActorListAdapter.kt
+++ app/src/main/java/me/liangfei/databinding/adapters/ActorListAdapter.kt
@@ -11,24 +11,39 @@ import androidx.recyclerview.widget.RecyclerView
 import me.liangfei.databinding.R
 import me.liangfei.databinding.data.entities.Actor
 import me.liangfei.databinding.databinding.ListItemActorBinding
+import me.liangfei.databinding.databinding.ListItemAddBinding
 import me.liangfei.databinding.fragments.ActorListFragmentDirections
 
 
-class ActorListAdapter : ListAdapter<Actor, ActorListAdapter.ViewHolder>(ActorDiffCallback()){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-                DataBindingUtil.inflate(
-                        LayoutInflater.from(parent.context),
-                        R.layout.list_item_actor, parent, false
+class ActorListAdapter : ListAdapter<Actor, RecyclerView.ViewHolder>(ActorDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            if (viewType.isTypeAdd()) {
+                AddViewHolder(
+                        DataBindingUtil.inflate(
+                                LayoutInflater.from(parent.context),
+                                R.layout.list_item_add, parent, false
+                        )
                 )
-        )
-    }
+            } else {
+                ViewHolder(
+                        DataBindingUtil.inflate(
+                                LayoutInflater.from(parent.context),
+                                R.layout.list_item_actor, parent, false
+                        )
+                )
+            }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        getItem(position).let { actor ->
-            with(holder) {
-                itemView.tag = actor
-                bind(createOnClickListener(actor.actorId), actor)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position).isTypeAdd()) {
+            (holder as AddViewHolder).bind(View.OnClickListener {
+                // TODO go to add more actors
+            })
+        } else {
+            getItem(position).let { actor ->
+                with(holder as ViewHolder) {
+                    itemView.tag = actor
+                    bind(createOnClickListener(actor.actorId), actor)
+                }
             }
         }
     }
@@ -40,16 +55,35 @@ class ActorListAdapter : ListAdapter<Actor, ActorListAdapter.ViewHolder>(ActorDi
         }
     }
 
+    override fun getItemViewType(position: Int) =
+            if (position == itemCount - 1) VIEW_TYPE_ADD else VIEW_TYPE_ACTOR
+
+
+    override fun getItemCount() = super.getItemCount() + 1
+
     class ViewHolder(
             private val binding: ListItemActorBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(listener: View.OnClickListener, actor: Actor) {
-            with (binding) {
+            with(binding) {
                 clickListener = listener
                 this.actor = actor
                 executePendingBindings()
             }
         }
+    }
+
+    class AddViewHolder(val binding: ListItemAddBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(listener: View.OnClickListener) {
+            binding.onClickListener = listener
+        }
+    }
+
+    companion object {
+        private const val VIEW_TYPE_ACTOR = 0x00
+        private const val VIEW_TYPE_ADD = 0x01
+
+        fun Int.isTypeAdd() = this == VIEW_TYPE_ADD
     }
 }
 
